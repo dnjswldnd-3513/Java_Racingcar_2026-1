95ad598eb111279b6995ca0573e59a6b593f8815
dnjswldnd-3513 <zheld3513@gmail.com>
2026-03-24 20:55:49 +0900

    feat-create-Race and initial setting

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,4 +1,31 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Race {
+
+    private List<Car> carList;
+    private int race_Count;
+
+    public Race() {
+    }
+
+    public void start(){
+        initCar();
+        initRace();
+    }
+
+    private void initRace() {
+    }
+
+    private void initCar() {
+        
+    }
+
+
 }
