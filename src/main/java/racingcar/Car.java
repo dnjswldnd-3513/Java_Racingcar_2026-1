0bae63fbc4f1c423431a9555ce9c827346179c52
dnjswldnd-3513 <zheld3513@gmail.com>
2026-03-24 20:52:20 +0900

    feat:create

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.stream.IntStream;
+
 public class Car {
+
+    private String name;
+    private int stack;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
 }
