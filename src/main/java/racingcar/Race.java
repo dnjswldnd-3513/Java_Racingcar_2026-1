572fa69a7a9d53b4cd044c85501aa89edcd75922
dnjswldnd-3513 <zheld3513@gmail.com>
2026-03-24 20:57:13 +0900

    feat-initCar- 자동차 이름 설정 및 에러 공백이 포함되었는지, 중복,길이 포함

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -24,7 +24,24 @@ public class Race {
     }
 
     private void initCar() {
-        
+        carList = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String text = camp.nextstep.edu.missionutils.Console.readLine();
+
+        for(String t : text.trim().split(",")){
+            if (!t.equals(t.trim())) throw new IllegalArgumentException("이름에 공백이 포함되었습니다.");
+
+            if (carList.stream().anyMatch(c -> c.getName().equals(t.trim()))){
+                throw new IllegalArgumentException();
+            }
+
+            if(t.length() > 5){
+                throw new IllegalArgumentException("이름의 길이가 잘못되었습니다.");
+            }
+            Car car = new Car(t.trim());
+            carList.add(car);
+        }
     }
 
 
